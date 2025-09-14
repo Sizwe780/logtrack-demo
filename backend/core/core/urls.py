b30e91535d37a2521358d09607a43e1241b94909
Sizwe Ngwenya <sizwe.ngwenya78@gmail.com>
2025-09-14 17:57:27 +0200

    Updated urls.py

--- backend/core/core/urls.py
+++ backend/core/core/urls.py
@@ -1,22 +1,17 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+# Import HttpResponse to return a simple response
+from django.http import HttpResponse
+
+# A simple view for a health check. This function will be called when
+# a request is made to the '/health/' endpoint.
+def health_check(request):
+    return HttpResponse("OK", status=200)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Add a new URL pattern for the health check.
+    # This is a common practice for load balancers and
+    # container orchestration systems to verify the service is running.
+    path('health/', health_check, name='health_check'),
 ]
