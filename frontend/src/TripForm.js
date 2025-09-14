53d9962d3f9ba6d5df966fdb36f1aa4c50230129
Sizwe Ngwenya <sizwe.ngwenya78@gmail.com>
2025-09-14 14:50:52 +0200

    Connecting the live backend to frontend

--- frontend/src/TripForm.js
+++ frontend/src/TripForm.js
@@ -34,7 +34,7 @@ function TripForm() {
     };
 
     try {
-      await axios.post('http://127.0.0.1:8000/api/trips/', tripData);
+      await axios.post(`${process.env.REACT_APP_API_URL}/api/trips/`, tripData);
       setOrigin('');
       setDestination('');
       setDate('');
