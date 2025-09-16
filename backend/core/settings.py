f58bca06be239950bc9cb42deb454ba68a0b700e
Sizwe Ngwenya <112629001+Sizwe780@users.noreply.github.com>
2025-09-16 15:32:48 +0200

    Update settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -92,7 +92,6 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles' # <<< ADDED THIS LINE
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
