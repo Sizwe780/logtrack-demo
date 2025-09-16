7bd16dc6e051e0ebf2434da70fcf9bb785d11c9c
Sizwe Ngwenya <112629001+Sizwe780@users.noreply.github.com>
2025-09-16 14:07:03 +0200

    Update urls.py

--- backend/trips/urls.py
+++ backend/trips/urls.py
@@ -1,13 +1,11 @@
+# Trips App: urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import TripViewSet
 
-# Create a router and register our viewsets with it.
 router = DefaultRouter()
 router.register(r'trips', TripViewSet, basename='trip')
 
-# The API URLs are now determined automatically by the router.
-# The `include(router.urls)` line handles all the routing for your API endpoints.
 urlpatterns = [
     path('', include(router.urls)),
 ]
